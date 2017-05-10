4d06e01f7d280b6303270e6ca7f88ea22cd49442
faizan.m@netree.in <faizan.m@netree.in>
2017-05-11 00:43:53 +0530

    register ui

--- app/src/main/java/com/oneminutebefore/workout/RegisterFragment.java
+++ app/src/main/java/com/oneminutebefore/workout/RegisterFragment.java
@@ -5,10 +5,13 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.Button;
+import android.widget.EditText;
 
 
 public class RegisterFragment extends Fragment {
+    private EditText etFirstName, etLastName, etMobileNo, etTimeZone, etLevel;
+    private Button btnRegister,btnSignIn;
 
 
     public RegisterFragment() {
@@ -17,9 +20,9 @@ public class RegisterFragment extends Fragment {
 
 
     // TODO: Rename and change types and number of parameters
-    public static RegisterFragment newInstance(String param1, String param2) {
-        RegisterFragment fragment = new RegisterFragment();
-        return fragment;
+    public static RegisterFragment newInstance() {
+        return new RegisterFragment();
+
     }
 
     @Override
@@ -32,10 +35,18 @@ public class RegisterFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false);
+        View fragmentView = inflater.inflate(R.layout.fragment_register, container, false);
+        etFirstName = (EditText) fragmentView.findViewById(R.id.et_first_name);
+        etLastName = (EditText) fragmentView.findViewById(R.id.et_last_name);
+        etMobileNo = (EditText) fragmentView.findViewById(R.id.et_mobile_no);
+        etTimeZone = (EditText) fragmentView.findViewById(R.id.et_time_zone);
+        etLevel = (EditText) fragmentView.findViewById(R.id.et_level);
+        btnRegister = (Button) fragmentView.findViewById(R.id.btn_register);
+        btnSignIn = (Button) fragmentView.findViewById(R.id.btn_login);
+        return fragmentView;
     }
 
-  }
+}
 
 
 
